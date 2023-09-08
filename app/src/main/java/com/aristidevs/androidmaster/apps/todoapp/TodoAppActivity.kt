f0fb29fe4968f43c0d33e2491ea1a7641954fe75
Emmanuel Fernandez <emmanuelfernandez600@gmail.com>
2023-09-08 11:36:20 -0300

    TODOAPP: Created initial files

---
+++ app/src/main/java/com/aristidevs/androidmaster/apps/todoapp/TodoAppActivity.kt
@@ -0,0 +1,12 @@
+package com.aristidevs.androidmaster.apps.todoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aristidevs.androidmaster.R
+
+class TodoAppActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todoapp)
+    }
+}
